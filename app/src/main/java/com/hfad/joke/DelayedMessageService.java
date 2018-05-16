7d3b3983306291383018f3f461783ada37556e1a
Monir Hossain <mknishad1@ymail.com>
2018-05-16 16:58:48 +0600

    log message

--- app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -2,6 +2,7 @@ package com.hfad.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 
 /**
@@ -12,12 +13,26 @@ import android.content.Intent;
  */
 public class DelayedMessageService extends IntentService {
 
+    public static final String EXTRA_MESSAGE = "message";
+
     public DelayedMessageService() {
         super("DelayedMessageService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        synchronized (this) {
+            try {
+                wait(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String text = intent.getStringExtra(EXTRA_MESSAGE);
+            showText(text);
+        }
+    }
 
+    private void showText(String text) {
+        Log.wtf("DelayedMessageService", "The message is: " + text);
     }
 }
